1cf93df1a94a8801ab1fd38722205d38856d1784
Markus Sahrakorpi <markus.sahrakorpi@gmail.com>
2017-12-10 21:12:23 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = require('express')();
 const bodyParser= require('body-parser');
 const fs = require('fs');
 const imgsFolder = './public/images/';
-
+const multer = require('multer');
+var Ajax = require('simple-ajax');
 global.jQuery = global.$ = require('jquery');
 
 var http = require('http').Server(app);
+var rhttp = require('http');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,11 +18,26 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); /
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
+ var Storage = multer.diskStorage({
+     destination: function(req, file, callback) {
+         callback(null, __dirname+'/public/images/');
+     },
+     filename: function(req, file, callback) {
+         callback(null, file.originalname);
+     }
+ });
+
+ var upload = multer({
+     storage: Storage
+ }).array("imgUploader", 100); //Field name and max count
+
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
+
+var config = JSON.parse(fs.readFileSync('config.json'), 'utf-8');
 
 function getImgData(){
 var imgData = [];
@@ -30,8 +47,21 @@ fs.readdirSync(imgsFolder).forEach(file => {
 return imgData;
 }
 
+function isWebsite(url){
+    console.log(url);
+}
+
+function writeChangeToConfig(){
+    var confStr = JSON.stringify(config);
+    console.log(confStr);
+    fs.writeFile('config.json', confStr, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+}
+
 app.get('/', (req, res) => {
-     res.render('pages/index', {data:getImgData()});
+     res.render('pages/index', {data:getImgData(), config: config});
 });
 
 app.get('/bg', (req, res)=>{
@@ -40,9 +70,37 @@ app.get('/bg', (req, res)=>{
     res.sendFile(__dirname+'/public/images/'+randomImg);
 });
 
+app.get('/options', (req, res) =>{
+  res.render('pages/options', {data:getImgData(),  config: config});    
+});
+
 app.post('/search', (req,res) =>{
-    console.log(req.body.search);
-    res.redirect('http://google.com/search?q='+req.body.search);
+    rhttp.get("http://"+req.body.search, function(resp, error) {
+      console.log("statusCode: ", resp.statusCode); // <======= Here's the status code
+      console.log("headers: ", resp.headers);
+
+       if (!error && res.statusCode != 404) {
+            res.redirect('http://'+req.body.search);
+        }
+    }).on('error', function(e) {
+      console.error(e);
+      res.redirect('http://google.com/search?q='+req.body.search);
+    });
+    
+});
+
+ app.post("/upload", function(req, res) {
+     upload(req, res, function(err) {
+         if (err) {
+             return res.end("Something went wrong!");
+         }
+         return res.redirect('/options');
+     });
+ });
+
+app.post("/timer", function(req, res) {
+    config.timer = req.body.timer;
+    writeChangeToConfig();
 });
 
 //app.use('/images', express.static(__dirname + '/Images'));
